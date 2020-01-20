03b6b55f9354abce07a0daceba37c71fb2dda9bd
Leo Ji <leo@leoji.dev>
2020-01-20 16:41:04 -0500

    init

---
+++ blog/2020-01-20-hello-world.md
@@ -0,0 +1,13 @@
+---
+id: hello-world
+title: Hello!
+author: Leo Ji
+author_title: Author of this guide
+author_url: https://github.com/theleoji
+author_image_url: https://avatars1.githubusercontent.com/u/43583069?s=400&v=4
+tags: [hello]
+---
+
+Blog features are powered by the blog plugin. Simple add files to the `blog` directory. It supports tags as well!
+
+Delete the whole directory if you don't want the blog features. As simple as that!
---
+++ docusaurus.config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  title: 'Publication 000: Design Rules',
+  tagline: 'An opinionated set of rules regarding design practices',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'theleoji', // Usually your GitHub org/user name.
+  projectName: 'design-points', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Design Rules',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg'
+      },
+      links: [
+        { to: 'docs/introduction', label: 'Docs', position: 'left' },
+        { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right'
+        }
+      ]
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1'
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2'
+            }
+          ]
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus'
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus'
+            }
+          ]
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog'
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus'
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus'
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      }
+    ]
+  ]
+}
